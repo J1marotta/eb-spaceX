723baf0ea3ebec78a3743db6c189f0d6763c090e
J1marotta <james.marotta@outlook.com>
2020-05-01 19:14:40 +1000

    server responds

--- server/app.js
+++ server/app.js
@@ -1,72 +1,74 @@
-const dbPool = require("./db");
-const express = require("express");
-const bodyParser = require("body-parser");
-const axios = require("axios");
-const app = express();
-const mysql = require("mysql");
+const dbPool = require('./db')
+const express = require('express')
+const bodyParser = require('body-parser')
+const axios = require('axios')
+const app = express()
+const mysql = require('mysql')
+const cors = require('cors')
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// don't break the server
+app.use((req, res, next) => {
+  try {
+    next()
+  } catch (e) {
+    res.sendStatus(500)
+  }
+})
 
-const baseURL = "https://api.spacexdata.com/v3";
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get("/", async (req, res) => {
-  axios.get("/").then((r) =>
-    res.send({
-      r: r.data.length,
-    })
-  );
-});
+const baseURL = 'https://api.spacexdata.com/v3'
 
-app.get("/landpads/:id", async (req, res) => {
-  const ourId = req.params.id;
+app.get('/capsules', async (req, res) => {
+  axios.get(`${baseURL}/capsules`).then((r) => res.send([r.data]))
+})
+
+app.get('/landpads/', async (req, res) => {
+  axios.get(`${baseURL}/landpads`).then((r) => res.send([r.data]))
+})
+
+app.get('/landpads/:id', async (req, res) => {
+  const ourId = req.params.id
   var sql = mysql.format(`SELECT * from spaceData WHERE id = ?`, [ourId])
-  
 
-  const dbData = await dbPool.query(`SELECT * from spaceData WHERE id = ?`, [
-    ourId,
-  ], (err, results, fields) => {
-    if(err) throw err
-    return results
-  })
-  
+  const dbData = await dbPool
+    .query(sql)
+    .then((data) =>
+      data.map(({ id, spaceItem }) => ({
+        id,
+        spaceItem: JSON.parse(spaceItem),
+      }))
+    )
 
-  console.log({ dbData, sql, ourId })
-  
-  
-    if (!dbData.length) {
+  if (!dbData.length) {
     axios
       .get(`${baseURL}/landpads/${ourId}`, { validateStatus: () => true })
       .then(async (r) => {
-        res.status(r.status);
-        await dbPool.query(
-          `INSERT into spaceData (id, spaceItem ) values (?, ?) `,
-          [ourId, r.data]
-        );
-        return res.send(r.data);
+        // trust the status of the api
+        res.status(r.status)
+        if (r.status >= 200 && r.status < 300) {
+          // if it's legit deposit it.
+          await dbPool.query(
+            `INSERT into spaceData (id, spaceItem ) values (?, ?) `,
+            [ourId, JSON.stringify(r.data)]
+          )
+        }
+        // send it to the front-end
+        return res.send(r.data)
       })
       .catch((err) => {
-        console.error(err);
-        return res.status(500);
-      });
+        console.error(err)
+        return res.status(500)
+      })
   } else {
-    return res.send({ data: dbData[0] });
+    // send it to front end from our db
+    return res.send({ data: dbData[0] })
   }
-});
+})
 
-// app.get(`/landpads/db/:id`, async (req, res) => {
-//     const ourId  = req.params.id
-
-//     await dbPool.query('INSERT into spaceData (id, spaceItem ) values (?, ?) ', [ourId, 'potatoes']  )
-//     return res.sendStatus(200)
-// })
-
-// app.get(`/landpads/db/get/:id`, async (req, res) => {
-
-//     return res.send(je)
-// })
-
-app.listen(4000);
+app.listen(4000)
 console.log(
   `Listening on port: 4000, wait for the development server to be up...`
-);
+)
